b8061e2d647e65c85bb296e67cd6faa713382b46
Richard Price <mrspondooli@gmail.com>
2021-05-10 21:00:53 +0100

    inital commit project setup removed storyboard refs

---
+++ ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SwiftUIKitCryptoApp
+//
+//  Created by Richard Price on 10/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+}
+
